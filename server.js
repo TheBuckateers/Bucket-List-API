f54b098665e5a40931403f87cba344ab2114abe2
Willem Jacobs <wamj58@gmail.com>
2021-09-04 19:16:31 -0500

    added photo credits

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 app.use(cors());
 const mongoose = require("mongoose");
 app.use(express.json());
-const BucketListModel = require('./models/bucketList');
+const BucketListModel = require("./models/bucketList");
 
 const PORT = process.env.PORT || 3001;
 
@@ -30,7 +30,6 @@ app.get("*", (req, res) => {
   res.status(404).send("Page not found");
 });
 
-
 // test seed the db
 // Seeding the database
 async function seed(req, res) {
@@ -41,41 +40,43 @@ async function seed(req, res) {
       countryName: "Colombia",
       countryLat: "4.0",
       countryLon: "-72.0",
-      email: "vbchomp@gmail.com" ,
+      email: "vbchomp@gmail.com",
     });
     const testBucketTwo = new BucketListModel({
       countryCode: "RE",
       countryName: "Reunion",
       countryLat: "-21.15",
       countryLon: "55.5",
-      email: "vbchomp@gmail.com" ,
+      email: "vbchomp@gmail.com",
     });
     const testBucketThree = new BucketListModel({
       countryCode: "PT",
       countryName: "Portugal",
       countryLat: "39.5",
       countryLon: "-8.0",
-      email: "vbchomp@gmail.com" ,
+      email: "vbchomp@gmail.com",
     });
     const testBucketFour = new BucketListModel({
       countryCode: "KZ",
       countryName: "Kazakhstan",
       countryLat: "48.0",
       countryLon: "68.0",
-      email: "vbchomp@gmail.com" ,
+      email: "vbchomp@gmail.com",
     });
     testBucketOne.save();
     testBucketTwo.save();
     testBucketThree.save();
     testBucketFour.save();
-    console.log('I have excersiiiiiiiiiiized the database', "http://localhost:3001/seed");
-
+    console.log(
+      "I have excersiiiiiiiiiiized the database",
+      "http://localhost:3001/seed"
+    );
   }
-  console.log('DB has been seeded');
-} 
+  console.log("DB has been seeded");
+}
 
 // MONGODB_URI is the MongoDB Atlas Server URI saved as variable in heroku ***
-// snagged directions on how to setup connection for Atlas Server from 
+// snagged directions on how to setup connection for Atlas Server from
 // https://github.com/vbchomp/seattle-code-301n22/tree/main/class-14
 
 // setup default db connection
@@ -83,11 +84,10 @@ async function seed(req, res) {
 mongoose.connect(`${process.env.DATABASE_URL}/bucketList`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-
+});
 
 // connect to the default db connection
-// const db = mongoose.connection;
+const db = mongoose.connection;
 // get notifications of connections errors
 
 db.on("error", (error) => console.error(error))
@@ -97,9 +97,7 @@ db.on("error", (error) => console.error(error))
     seed();
     // get notified when connected to db
     db.once("open", () => console.log("Connected to Database"));
-  })
-
-
+  });
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
